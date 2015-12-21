6b1b6314dbe6fe317f8716c693960250a9e1a21c
darinh <darin@codenet.io>
2015-12-21 15:35:45 -0800

    Finished Bootstrap module.

---
+++ src/TheWorld/Services/DebugMailService.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Diagnostics;
+
+namespace TheWorld.Services
+{
+    public class DebugMailService : IMailService
+    {
+        #region Implementation of IMailService
+
+        public bool SendMail(string to, string @from, string subject, string body)
+        {
+            Debug.WriteLine($"Sending mail: To: {to}, Subject: {subject}");
+            return true;
+        }
+
+        #endregion
+    }
+
+}
--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -5,17 +5,38 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+using TheWorld.Services;
 
 namespace TheWorld
 {
     public class Startup
     {
+        public static IConfigurationRoot Configuration;
+
+        public Startup(IApplicationEnvironment appEnv)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(appEnv.ApplicationBasePath)
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+#if DEBUG
+            services.AddScoped<IMailService, DebugMailService>();
+#else
+            services.AddScoped<IMailService, RealMailService>();
+#endif
             // for dependency injection.  services collection (container) gets its services here.
         }
 
@@ -29,10 +50,10 @@ namespace TheWorld
                     name: "Default",
                     template: "{controller}/{action}/{id?}",
                     defaults: new
-                              {
-                                  controller = "App",
-                                  action = "Index"
-                              }
+                    {
+                        controller = "App",
+                        action = "Index"
+                    }
                     );
             });
             //app.Run(async (context) =>
